6b5f343d94e1a0b483d89379dcd45f28210336ac
katie19kl <59045304+katie19kl@users.noreply.github.com>
2019-12-29 11:03:07 +0200

    Add files via upload
    ConditionParser has a default constructor. therefore, there's no need to change anything in it.
    these are the only changed ones^

---
+++ PrintCommand.h
@@ -0,0 +1,25 @@
+//
+// Created by katie on 28/12/2019.
+//
+
+#ifndef PROJECT__PRINTCOMMAND_H_
+#define PROJECT__PRINTCOMMAND_H_
+
+#include <unordered_map>
+#include "Command.h"
+#include "Var.h"
+#include "SymboleTable.h"
+
+class PrintCommand: public Command {
+ private:
+  unordered_map<string, Var*> * name_to_var;
+
+ public:
+  explicit PrintCommand(SymboleTable symbol_table) {
+    this->name_to_var = symbol_table.get_NameVar_ToVar();
+  }
+  int execute(vector <string> tokens, int index) override;
+
+};
+
+#endif //PROJECT__PRINTCOMMAND_H_
